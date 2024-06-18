9bc10c220eca7dcd739e8f6589b57c1d777c982f
VictorZhanUnity <73773024+VictorZhanUnity@users.noreply.github.com>
2024-06-18 10:00:25 +0800

    Update

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "TCIT-iDCIM_PRIMAX-0.1";
+const contentToCache = [
+    "Build/iDCIM_PRIMAX.loader.js",
+    "Build/iDCIM_PRIMAX.framework.js.unityweb",
+    "Build/iDCIM_PRIMAX.data.unityweb",
+    "Build/iDCIM_PRIMAX.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
